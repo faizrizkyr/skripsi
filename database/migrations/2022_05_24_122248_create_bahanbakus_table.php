2dd47d55e6623b51c660cc04c562b6e099ae657d
faizrizkyr <faizrizkyr@gmail.com>
2022-05-24 19:38:54 +0700

    first commit

---
+++ database/migrations/2022_05_24_122248_create_bahanbakus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bahanbakus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->text('deskripsi')->nullable();
+            $table->bigInteger('harga')->default(0);
+            $table->integer('min_stok')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bahanbakus');
+    }
+};
