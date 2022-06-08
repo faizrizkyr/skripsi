15b4a9ef953747626d0311990e7c552c8d5464e7
faizrizkyr <faizrizkyr@gmail.com>
2022-06-08 15:13:46 +0700

    menambahkan navbar utama

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Coba</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'tentang' ? 'active' : '' }}" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'blog' ? 'active' : '' }}" href="/blog">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ $active === 'categories' ? 'active' : '' }}"
+                        href="/categories">Categories</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-circle"></i> 
+                            Welcome back, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-window-reverse"></i> My Dashboard</a></li>
+                            {{-- <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li> --}}
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                              <form action="/logout" method="post">
+                                @csrf
+                                <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                              </form>
+                              </li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}"><i
+                                class="bi bi-person-check"></i> Login</a>
+                    </li>
+                </ul>
+            @endauth
+        </div>
+    </div>
+</nav>
