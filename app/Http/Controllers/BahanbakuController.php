e993abc237275e92c77b80fe9c3ec7e81bd27fcf
faizrizkyr <faizrizkyr@gmail.com>
2022-06-09 00:08:37 +0700

    added edit function on BahanbakuController

--- app/Http/Controllers/BahanbakuController.php
+++ app/Http/Controllers/BahanbakuController.php
@@ -69,9 +69,11 @@ class BahanbakuController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Bahanbaku $post)
     {
-        //
+        return view('dashboard.posts.bb_edit', [
+            'post' => $post
+        ]);
     }
 
     /**
