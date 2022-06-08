522dcc879130c6e18495d2e08a7b83d41267c142
faizrizkyr <faizrizkyr@gmail.com>
2022-06-08 23:14:18 +0700

    add function index to bahanbaku controller

--- app/Http/Controllers/BahanbakuController.php
+++ app/Http/Controllers/BahanbakuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bahanbaku;
 use Illuminate\Http\Request;
 
 class BahanbakuController extends Controller
@@ -13,7 +14,9 @@ class BahanbakuController extends Controller
      */
     public function index()
     {
-        //
+        return view('dashboard.posts.bb_index', [
+            'posts' => Bahanbaku::all()
+        ]);
     }
 
     /**
