d11337fdccab8e099f8afcbadfce8905cfd5759f
faizrizkyr <faizrizkyr@gmail.com>
2022-06-11 13:09:13 +0700

    added Model, Controller & Migration to Pemesanan

---
+++ database/migrations/2022_06_11_060226_create_pemesanans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bahanbaku_id');
+            $table->integer('jumlah');
+            $table->dateTime('tgl_pemesanan');
+            $table->timestamps();
+            $table->foreign('bahanbaku_id')->references('id')->on('bahanbakus')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+};
