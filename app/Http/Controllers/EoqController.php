0a87959122b5a0699a9b9c930d4d64053b86c531
faizrizkyr <faizrizkyr@gmail.com>
2022-06-22 17:10:17 +0700

    added function hitung & store

--- app/Http/Controllers/EoqController.php
+++ app/Http/Controllers/EoqController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bahanbaku;
+use App\Models\Eoq;
 use Illuminate\Http\Request;
 
 class EoqController extends Controller
@@ -26,7 +27,7 @@ class EoqController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function hitung(Request $request)
     {
         $validatedData = $request->validate([
             'tahun' => 'required',
@@ -47,7 +48,47 @@ class EoqController extends Controller
         $frekuensi = round($d/$eoq);
 
         $T = round(269/$frekuensi);
-        dd($eoq, $frekuensi, $T);
+        $tgl_awal = date('d F Y', strtotime("$request->tahun-1-1") );
+        $interval[0] = $tgl_awal;
+
+        for ($i=1; $i < $frekuensi; $i++) { 
+            $tgl_awal = date('d F Y', strtotime($tgl_awal . " +$T days"));
+            
+            $interval[$i]=$tgl_awal;
+        }
+        // dd($eoq, $frekuensi, $T);
+
+        
+        return view('admin.eoq.results', [
+            'tahun' => $request->tahun,
+            'bahanbaku' => Bahanbaku::findOrFail($request->bahanbaku_id),
+            'holding_cost' => $request->holding_cost,
+            'jml_kebutuhan' => $d,
+            'biaya_order' => $s,
+            'eoq' => $eoq,
+            'frekuensi' => $frekuensi,
+            'interval' => $T,
+            'tglInterval' => $interval
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'tahun' => 'required',
+            'bahanbaku_id' => 'required',
+            'holding_cost' => 'required',
+            'jml_kebutuhan' => 'required',
+            'biaya_order' => 'required',
+            'eoq' => 'required',
+            'frekuensi_order' => 'required',
+            'interval_order' => 'required'
+        ]);
+
+        Eoq::updateOrCreate(['bahanbaku_id' => $request->bahanbaku_id, 'tahun' => $request->tahun], $validatedData);
+
+        return redirect('/admin/bahanbaku/'. $request->bahanbaku_id)->with('success', ' Berhasil menghitung EOQ.');
+
     }
 
 
