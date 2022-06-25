c8c1627c151075504447d93e4c9ea85f635de240
faizrizkyr <faizrizkyr@gmail.com>
2022-06-25 19:33:00 +0700

    add failed attempt

--- resources/views/admin/pemakaian/create.blade.php
+++ resources/views/admin/pemakaian/create.blade.php
@@ -1,6 +1,12 @@
 @extends('admin.layouts.main')
 
 @section('container')
+
+@if (session()->has('failed'))
+<div class="alert alert-danger col-lg-9" role="alert">
+  {{ session('failed') }}
+</div>
+@endif
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Tambah Data Pemakaian Bahan Baku</h1>
     </div>
