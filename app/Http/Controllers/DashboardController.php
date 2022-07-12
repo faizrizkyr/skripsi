fed56d6300cbcee62f634899b1cdf481977bdc56
Mochamad Nor Fadillah <mochamadfadilah88@gmail.com>
2022-07-12 21:06:00 +0700

    add dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Bahanbaku;
+use App\Models\Pemakaian;
+use App\Models\Pemesanan;
+use App\Models\Sparepart;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $jumlah = [
+            'bahanbaku' => Bahanbaku::count(),
+            'pemakaian' => Pemakaian::count(),
+            'pemesanan' => Pemesanan::count(),
+            'sparepart' => Sparepart::count(),
+        ];
+
+        return view('admin.index', compact('jumlah'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,71 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Selamat Datang Kembali, {{ auth()->user()->name }}!!</h1>
     </div>
+
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="card border-0 bg-success text-white">
+                <div class="card-body">
+                    <div class="d-flex bd-highlight">
+                        <div class="p-2 flex-grow-1 bd-highlight">
+                            <h3>{{ $jumlah['bahanbaku'] }}</h3>
+                            <p>Bahan Baku</p>
+                        </div>
+                        <div class="p-2 bd-highlight my-auto">
+                            {{-- <span class="fs-2" data-feather="database"></span> --}}
+                            <i class="fa fa-boxes-stacked fs-1"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card border-0 bg-danger text-white">
+                <div class="card-body">
+                    <div class="d-flex bd-highlight">
+                        <div class="p-2 flex-grow-1 bd-highlight">
+                            <h3>{{ $jumlah['pemakaian'] }}</h3>
+                            <p>Sparepart</p>
+                        </div>
+                        <div class="p-2 bd-highlight my-auto">
+                            {{-- <span class="fs-2" data-feather="database"></span> --}}
+                            <i class="fa fa-gears fs-1"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card border-0 bg-primary text-white">
+                <div class="card-body">
+                    <div class="d-flex bd-highlight">
+                        <div class="p-2 flex-grow-1 bd-highlight">
+                            <h3>{{ $jumlah['pemesanan'] }}</h3>
+                            <p>Pemesanan</p>
+                        </div>
+                        <div class="p-2 bd-highlight my-auto">
+                            {{-- <span class="fs-2" data-feather="database"></span> --}}
+                            <i class="fa fa-angles-left fs-1"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card border-0 bg-warning text-white">
+                <div class="card-body">
+                    <div class="d-flex bd-highlight">
+                        <div class="p-2 flex-grow-1 bd-highlight">
+                            <h3>{{ $jumlah['sparepart'] }}</h3>
+                            <p>Pemakaian</p>
+                        </div>
+                        <div class="p-2 bd-highlight my-auto">
+                            {{-- <span class="fs-2" data-feather="database"></span> --}}
+                            <i class="fa fa-angles-right fs-1"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
